fc14adbf946b8786b3b87448c59a50acbd145305
rupalkatre133 <rupalkatre133@gmail.com>
2026-04-10 09:52:47 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,24 @@
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+variable "region" {
+  default = "us-east-1"
 }
 
-variable "ami_id" {
-  default = "ami-0ec10929233384c7f" # Ubuntu AMI
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 
 variable "instance_type" {
   default = "t3.small"
 }
 
-variable "key_name" {
-  description = "SSH key pair name"
+variable "ami_id" {
+  default = "ami-0ec10929233384c7f"
 }
 
+variable "key_name" {
+  description = "Your EC2 key pair"
+}
+
+variable "allowed_ip" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
