72f2c95e7be8f2656f002d1953ffd347153dd01a
rupalkatre133 <rupalkatre133@gmail.com>
2026-04-10 09:32:04 +0530

    Add outputs for Jenkins and EKS cluster details

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
 
