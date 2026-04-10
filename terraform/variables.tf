38e53b770f062b4666a3abcbee3dfa4ca1355060
rupalkatre133 <rupalkatre133@gmail.com>
2026-04-10 09:31:03 +0530

    Add variables for VPC, AMI, instance type, and key name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,16 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "ami_id" {
+  default = "ami-0ec10929233384c7f" # Ubuntu AMI
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+}
 
