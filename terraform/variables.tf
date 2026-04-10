91fc44426fbaf03d10ce93bf9fd239945a9f9950
rupalkatre133 <rupalkatre133@gmail.com>
2026-04-10 11:01:14 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,19 +6,18 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "instance_type" {
-  default = "t3.small"
-}
-
 variable "ami_id" {
   default = "ami-0ec10929233384c7f"
 }
 
+variable "instance_type" {
+  default = "t3.small"
+}
+
 variable "key_name" {
-  description = "Your EC2 key pair"
+  default = "pemvirginia2"
 }
 
 variable "allowed_ip" {
-  type    = list(string)
   default = ["0.0.0.0/0"]
 }
