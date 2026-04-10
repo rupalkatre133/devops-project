ea6e0365053eb29c64902a1a515f433d29b1fc83
rupalkatre133 <rupalkatre133@gmail.com>
2026-04-10 09:53:56 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,11 +2,10 @@ output "jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
 output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
